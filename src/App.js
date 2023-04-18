95faf5f4103d209761c7cd26dd68395422fd4d8f
Birhat03 <birhat2003@hotmail.com>
2023-04-18 18:17:34 +0200

    Fiksa på layout og semantikk

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Route, Routes} from 'react-router-dom'
+import Layout from './components/Layout';
+import './App.css';
+
+function App() {
+  return (
+    <Layout/>
+  );
+}
+
+export default App;
